bb2484270da23dc51bd99e0e60b86f1c2a2f1020
Arcel-Bika <arcelbika@gmail.com>
2023-12-01 20:44:02 +0100

    Update 5: add vowels_compter

---
+++ main/beginner/vowels_compter.py
@@ -0,0 +1,30 @@
+"""
+Enoncé:
+    Créer un compteur des voyelles
+
+"""
+
+
+def vowels_compter(sentence):
+    vowels_list = ["a", "e", "i", "o", "u"]
+
+    sentence_list = sentence.lower().split()
+    sentence_list = "".join(sentence_list)
+    compter, j, i = 0, 0, 0
+
+    for i in range(len(sentence_list)):
+        for v, w in enumerate(vowels_list):
+            if sentence_list[i] == vowels_list[v]:
+                compter += 1
+        i += 1
+
+    if compter == 0:
+        print("Il n'y a aucune voyelle dans votre phrase")
+    elif compter == 1:
+        print("Il y a une voyelle dans votre phrase")
+    else:
+        print(f"Il y a {compter} voyelles dans votre phrase")
+
+
+vowels_compter(sentence = input("Entrer une phrase: "))
+

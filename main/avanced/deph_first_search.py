b2bbe49f00d787772cfe0f56229230ba0efe5515
Arcel-Bika <arcelbika@gmail.com>
2024-03-01 23:28:12 +0100

    Update 6: Creation of deph first search

---
+++ main/avanced/deph_first_search.py
@@ -0,0 +1,5 @@
+"""
+Description:
+    L'algorithme de recherche en profondeur d'abord (DFS - Depth-First Search en anglais),
+    de parcours de graphe largement utilisé en informatique et en sciences de l'ingénieur.
+"""
